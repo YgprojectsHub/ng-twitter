cca83b77cd3eb5eff4ec57dda1c6e42c7effcb46
Yusuf Güngörür <89793711+YgprojectsHub@users.noreply.github.com>
2023-11-16 22:44:12 +0300

    Update environments.ts

--- ng-twitter/twitter/src/environments/environments.ts
+++ ng-twitter/twitter/src/environments/environments.ts
@@ -1,12 +1,12 @@
 
 export const environment = {
     firebaseConfig:{
-        apiKey: "AIzaSyDtl2Bq2_vqtgnyXFnFeMA9JrIzoQYK1Ao",
+        apiKey: "YOUR_KEY",
         authDomain: "ng-twitter-bbcfd.firebaseapp.com",
         projectId: "ng-twitter-bbcfd",
         storageBucket: "ng-twitter-bbcfd.appspot.com",
-        messagingSenderId: "885721637683",
-        appId: "1:885721637683:web:047466b26847f106c3d446",
-        measurementId: "G-GGSKZ8M871"
+        messagingSenderId: "YOUR-MESSAGING-ID",
+        appId: "YOUR_ID",
+        measurementId: "YOUR-ID"
     }
 }
